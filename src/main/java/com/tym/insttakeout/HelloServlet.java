1f96197cb016f49f328845ba5fda706837308136
Tianyao Ma <tianyao.ma333@gmail.com>
2021-11-06 18:45:10 -0400

    set up doGet and doPost function for users to register, and build new customer class in entity package

--- src/main/java/com/tym/insttakeout/HelloServlet.java
+++ src/main/java/com/tym/insttakeout/HelloServlet.java
@@ -1,5 +1,10 @@
 package com.tym.insttakeout;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tym.insttakeout.entity.Customer;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
 import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,17 +14,43 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Hello Vincent!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+        response.setContentType("application/json");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        ObjectMapper mapper = new ObjectMapper();
+        Customer customer = new Customer.CustomerBuilder().
+                email("lee@gamil.com")
+                .firstName("Fiona")
+                .lastName("Lee")
+                .password("123456")
+                .enabled(false)
+                .build();
+
+        response.getWriter().print(mapper.writeValueAsString(customer));
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // read customer information from request body
+        // request.getReader() retrieve the body info from http request;
+        JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
+        String email = jsonRequest.getString("email");
+        String firstName = jsonRequest.getString("first_name");
+        String lastName = jsonRequest.getString("last_name");
+        int age = jsonRequest.getInt("age");
+        //print customer information to IDE console:
+        System.out.println("Email is " + email);
+        System.out.println("First name is " + firstName);
+        System.out.println("Last name is " + lastName);
+        System.out.println("Age is " + age);
+
+        // Return status = ok as response body to the client
+        response.setContentType("application/json");
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("status", "ok");
+        response.getWriter().print(jsonResponse);
     }
 
     public void destroy() {
---
+++ src/main/java/com/tym/insttakeout/entity/Customer.java
@@ -0,0 +1,83 @@
+package com.tym.insttakeout.entity;
+
+public class Customer {
+    private String email;
+    private final String firstName;
+    private final String lastName;
+    private String password;
+    private boolean enabled;
+
+    private Customer(CustomerBuilder builder) {
+        this.email = builder.email;
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.password = builder.password;
+        this.enabled = builder.enabled;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+    public static class CustomerBuilder{
+        private String email;
+        private String firstName;
+        private String lastName;
+        private String password;
+        private boolean enabled;
+
+        public CustomerBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+        public CustomerBuilder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+        public CustomerBuilder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+        public CustomerBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+        public CustomerBuilder enabled(boolean enabled) {
+            this.enabled = enabled;
+            return this;
+        }
+        public Customer build() {
+            if (email == null || firstName == null || lastName == null) {
+                throw new IllegalArgumentException("required fields are not set!");
+            }
+            return new Customer(this);
+        }
+    }
+}
