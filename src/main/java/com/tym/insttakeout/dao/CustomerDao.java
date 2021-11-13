597d42a9a36387b242e10ceadb339acc48701abf
Tianyao Ma <tianyao.ma333@gmail.com>
2021-11-13 11:41:25 -0500

    added context-param for initializeing the spring ioc container on web app

--- src/main/java/com/tym/insttakeout/dao/CustomerDao.java
+++ src/main/java/com/tym/insttakeout/dao/CustomerDao.java
@@ -1,14 +1,16 @@
 package com.tym.insttakeout.dao;
 
 import com.tym.insttakeout.entity.Customer;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class CustomerDao {
     public void signUp(Customer customer) {
 
     };
 
     public Customer getCustomer(String email) {
-        return new Customer();
+        return new Customer.CustomerBuilder().build();
     }
 
 }
