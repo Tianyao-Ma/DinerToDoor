bf30613c893cac20ddddae4509e259a2170fc225
Tianyao Ma <tianyao.ma333@gmail.com>
2021-11-20 13:42:45 -0500

    setup signIn controller to deal with failed login request

---
+++ src/main/java/com/tym/insttakeout/controller/SignInController.java
@@ -0,0 +1,28 @@
+package com.tym.insttakeout.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class SignInController {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    // we only process the failed login request here, if success, it will redirect
+    // to login page
+    @RequestMapping("/login")
+    public void loginFailureHandler(@RequestParam(value="error") String error, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        Map<String, Object> data = new HashMap<>();
+        data.put("message", "bad credentials");
+        response.getOutputStream()
+                .println(objectMapper.writeValueAsString(data));
+    }
+
+}
